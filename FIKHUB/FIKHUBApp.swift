d0eeb3e5518c10deb04ef08c2869405478946839
Rangga Biner <ranggabiner@gmail.com>
2024-10-05 19:19:37 +0700

    Initial Commit

---
+++ FIKHUB/FIKHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIKHUBApp.swift
+//  FIKHUB
+//
+//  Created by Rangga Biner on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FIKHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
